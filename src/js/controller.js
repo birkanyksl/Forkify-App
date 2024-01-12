6199b9c54f81a592227c207a95e2928726c303d9
Birkan <birkanyksl613@gmail.com>
2024-01-12 04:59:05 +0300

    Forkify App
    Recipe App

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,11 +31,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    // Gettin Hash ID #xxx
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886`
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -159,4 +163,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
