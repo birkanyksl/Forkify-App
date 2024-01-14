1c377f0e39f2a01bf07b87e480c0d8ab2d04e163
Birkan <birkanyksl613@gmail.com>
2024-01-14 05:57:19 +0300

    Forkify App
    Recipe App

--- src/js/model.js
+++ src/js/model.js
@@ -24,6 +24,7 @@ export const loadRecipe = async function (id) {
     console.log(state.recipe);
   } catch (err) {
     // Temp error handling
-    console.err(`${err}😈😈`);
+
+    throw err;
   }
 };
