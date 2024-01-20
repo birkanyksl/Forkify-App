c9e7920d4dea50e31a20c7efe8f5d90035327362
Birkan <birkanyksl613@gmail.com>
2024-01-20 06:35:11 +0300

    Forkify App
    Implemented search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable'; // polifiling rest
 import 'regenerator-runtime/runtime'; // polifiling single await
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -29,6 +34,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -36,6 +43,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
     // 3) Render results
     console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
